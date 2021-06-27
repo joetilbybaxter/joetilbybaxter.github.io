88697d6d2749f40d1d95a9f66de335b8b884ba60
JakeTB <jtilbybaxter@gmail.comgit config --global user.name Jakegit config --global user.email jtilbybaxter@gmail.com>
2021-06-27 13:21:59 +0100

    First draft completed with added contact page

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,9 +1,30 @@
 import "./contacts.scss"
+import PhoneIcon from '@material-ui/icons/Phone';
+import {useState} from "react"
 
 export default function Contacts() {
+
+    const [message, setMessage] = useState(false)
+
+    const handleSubmit = (e)=> {
+
+        e.preventDefault();
+        setMessage(true)
+    }
     return(
         <div className="contacts" id="contact">
-            sss
+            <div className="left">
+                <img src="assets/handsake1.png"></img>
+            </div>
+            <div className="right">
+                <h2>Contact.</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Email"/>
+                    <textarea placeholder="Message"></textarea>
+                    <button type="">Send</button>
+                    {message && <span>Thankyou for the message, I will get back in touch soon!</span>}
+                </form>
+            </div>
         </div>
     )
 }
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,7 +3,20 @@ import "./testimonials.scss"
 export default function Testimonials() {
     return(
         <div className="testimonials" id="testimonials">
-
+            <div className="container">
+                <div className="card">
+                    <div className="top">
+                        <img src="" alt="" />
+                        <img className="user" src="" alt="" />
+                        <img className="" src="" alt="" />
+                    </div>
+                    <div className="center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti maxime tempora consectetur beatae? Nihil eius aspernatur quasi iste quod! Reiciendis, error natus? Cumque, veritatis commodi! Tenetur dicta vel aliquam temporibus!</div>
+                    <div className="bottom">
+                        <h3>Alex</h3>
+                        <h3>CEO of code</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
